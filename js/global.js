bad7b9cf25294d2d7443f270f07fb5aa0143ecdd
noneistherekk <154257889+noneistherekk@users.noreply.github.com>
2023-12-18 17:49:27 +0100

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,19 @@
+﻿function getErrorMessage(jqXHR, exception) {
+    var msg = '';
+    if (jqXHR.status === 0) {
+        msg = 'Pas de connexion.\n Vérifiez le réseau Internet.';
+    } else if (jqXHR.status == 404) {
+        msg = 'Page demandée introuvable. [404]';
+    } else if (jqXHR.status == 500) {
+        msg = 'Erreur Interne du Serveur [500].';
+    } else if (exception === 'parsererror') {
+        msg = 'Échec de l\'analyse JSON demandée.';
+    } else if (exception === 'timeout') {
+        msg = 'Erreur de timeout.';
+    } else if (exception === 'abort') {
+        msg = 'Demande Ajax abandonnée.';
+    } else {
+        msg = jqXHR.responseText;
+    }
+    return msg;
+}
---
+++ js/gridmvc.lang.fr.js
@@ -0,0 +1,22 @@
+﻿/***
+* Grid.Mvc  language (fr-FR) http://gridmvc.codeplex.com/
+*/
+window.GridMvc = window.GridMvc || {};
+window.GridMvc.lang = window.GridMvc.lang || {};
+GridMvc.lang.fr = {
+    filterTypeLabel: "Type: ",
+    filterValueLabel: "Valeur:",
+    applyFilterButtonText: "Applique",
+    filterSelectTypes: {
+        Equals: "Egale",
+        StartsWith: "commance par",
+        Contains: "contient",
+        EndsWith: "fini par",
+        GreaterThan: "plus que",
+        LessThan: "moin que"
+    },
+    code: 'fr',
+    boolTrueLabel: "Vrai",
+    boolFalseLabel: "Faux",
+    clearFilterLabel: "Vide"
+};
